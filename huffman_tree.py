a21868debc6a1705d5e5b1b6e2ddd4a45555d9d4
Devyanshu <devyanshuhaha@gmail.com>
2019-11-26 11:25:02 +0530

    switched to classes

--- huffman_tree.py
+++ huffman_tree.py
@@ -1,39 +1,42 @@
 from binarytree import Node
 
+
+class HuffmanCoding:
+
+    def __init__(self, data):
+        self.data = data
+        self.total = sum(list(self.data.values()))
+
+    def get_max_key(self):
+        max_key = max(self.data, key=self.data.get)
+        return max_key, self.data[max_key]
+
+    def add_nodes(self, node):
+        while True:
+            if len(self.data.values()) == 1:
+                break
+            key, val = self.get_max_key()
+            node.left = Node(val)
+            del self.data[key]
+            self.total = self.total - val
+            node.right = Node(self.total)
+            self.add_nodes(node.right)
+
+    def print_tree(self):
+        root = Node(self.total)
+        self.add_nodes(root)
+
+        print(root)
+
+
 data = {
-    1: 14,
+    1: 144,
     2: 20,
     3: 89,
-    4: 10
+    4: 10,
+    5: 2
 }
 
-total = sum(list(data.values()))
 
-
-def get_max_key(dict):
-    max_key = max(dict, key=dict.get)
-    return max_key, dict[max_key]
-
-
-def add_nodes(node, data):
-    global total
-    while True:
-        if len(data.values()) == 1:
-            break
-        key, val = get_max_key(data)
-        print(key, val)
-        node.left = Node(val)
-        del data[key]
-        total = total - val
-        node.right = Node(total)
-        add_nodes(node.right, data)
-
-
-def print_tree(data):
-    root = Node(total)
-    add_nodes(root, data)
-
-    print(root)
-
-
-print_tree(data)
+hc = HuffmanCoding(data)
+hc.print_tree()
