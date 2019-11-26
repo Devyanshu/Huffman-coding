1415fad4bfdedfb6eafe0c2894bf695036bd46cb
Devyanshu <devyanshuhaha@gmail.com>
2019-11-26 11:11:03 +0530

    added files

---
+++ huffman_tree.py
@@ -0,0 +1,33 @@
+from binarytree import Node
+
+data = {
+    1:14,
+    2:20,
+    3:89,
+    4:10
+}
+
+total = sum(list(data.values()))
+
+def get_max_key(dict):
+    max_key = max(dict, key=dict.get)
+    return max_key, dict[max_key]
+
+def add_nodes(node, data):
+    global total    
+    while data:
+        key, val = get_max_key(data)
+        print(key, val)
+        node.left = Node(key)
+        del data[key]
+        total = total - val
+        node.right = Node(total)
+        add_nodes(node.right, data)
+
+def print_tree(data):
+    root = Node(total)
+    add_nodes(root, data)
+
+    print(root) 
+print_tree(data)
+
