2940a266170297484abb7df6a357e4f8b2c70658
Devyanshu <devyanshuhaha@gmail.com>
2019-11-26 11:15:00 +0530

    fixed last node

--- huffman_tree.py
+++ huffman_tree.py
@@ -1,33 +1,39 @@
 from binarytree import Node
 
 data = {
-    1:14,
-    2:20,
-    3:89,
-    4:10
+    1: 14,
+    2: 20,
+    3: 89,
+    4: 10
 }
 
 total = sum(list(data.values()))
+
 
 def get_max_key(dict):
     max_key = max(dict, key=dict.get)
     return max_key, dict[max_key]
 
+
 def add_nodes(node, data):
-    global total    
-    while data:
+    global total
+    while True:
+        if len(data.values()) == 1:
+            break
         key, val = get_max_key(data)
         print(key, val)
-        node.left = Node(key)
+        node.left = Node(val)
         del data[key]
         total = total - val
         node.right = Node(total)
         add_nodes(node.right, data)
 
+
 def print_tree(data):
     root = Node(total)
     add_nodes(root, data)
 
-    print(root) 
-print_tree(data)
+    print(root)
 
+
+print_tree(data)
